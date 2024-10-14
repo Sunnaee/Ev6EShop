1f34dec8c5c09a19babd18d26a1d14f32e89fca3
martingci <m.carrasco29@ufromail.cl>
2024-10-14 18:21:13 -0300

    Se corrigen los enum para los estados del pedido y cuenta web.

--- src/main/java/EstadoPedido.java
+++ src/main/java/EstadoPedido.java
@@ -1,2 +1,13 @@
 public enum EstadoPedido {
+    NUEVO("Nuevo"),
+    CREADO("Creado"),
+    DESPACHADO("Despachado"),
+    RECIBIDO("Recibido"),
+    CERRADO("Cerrado");
+
+    private String status;
+
+    EstadoPedido(String status) {
+        this.status = status;
+    }
 }
--- src/main/java/EstadoWeb.java
+++ src/main/java/EstadoWeb.java
@@ -1,2 +1,12 @@
 public enum EstadoWeb {
+    NUEVO("Nuevo"),
+    ACTIVO("Activo"),
+    BLOQUEADO_TEMPORALMENTE("Bloqueado temporalmente"),
+    PROHIBIDO("Prohibido");
+
+    private String status;
+
+    EstadoWeb(String status) {
+        this.status = status;
+    }
 }
